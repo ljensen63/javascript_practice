753346432241d36fc03b8bdf5a7d06882a4cef1b
Liam Jensen <jensen97@purdue.edu>
2022-06-28 23:13:42 -0400

    finish first part of practice for DOM manipulation and events section

---
+++ js-file.js
@@ -0,0 +1,18 @@
+const para = document.createElement('p');
+const h3 = document.createElement('h3');
+const div = document.createElement('div');
+const h1 = document.createElement('h1');
+const para2 = document.createElement('p');
+document.body.appendChild(para);
+document.body.appendChild(h3);
+div.appendChild(h1);
+div.appendChild(para2);
+document.body.appendChild(div);
+para.textContent = 'Hey! I\'m red';
+para.style.color = 'red';
+h3.textContent = 'I\'m a blue h3!'
+h3.style.color = 'blue';
+div.style.border = '1px solid black';
+div.style.backgroundColor = 'pink';
+h1.textContent = 'I\'m in a div';
+para2.textContent = 'ME TOO!';
